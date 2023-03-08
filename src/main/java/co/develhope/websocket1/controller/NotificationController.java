9139af0c419607f76d6b2628fd0483255e32a341
AndreaSciscioli <113891464+AndreaSciscioli@users.noreply.github.com>
2023-03-08 17:07:56 +0100

    commit

---
+++ src/main/java/co/develhope/websocket1/controller/NotificationController.java
@@ -0,0 +1,22 @@
+package co.develhope.websocket1.controller;
+
+import co.develhope.websocket1.entities.MessageDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+    @PostMapping("/notification")
+    public ResponseEntity sendNotification(@RequestBody MessageDTO messageDTO){
+        simpMessagingTemplate.convertAndSend("/topic/broadcast", messageDTO);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+}
